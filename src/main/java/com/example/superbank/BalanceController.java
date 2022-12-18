36339bc5471e110b0569b872a5dd6b819ccfa66c
Vyacheslav <kirillov.com@gmail.com>
2022-12-18 12:39:45 +0300

    Initial commit

---
+++ src/main/java/com/example/superbank/BalanceController.java
@@ -0,0 +1,43 @@
+package com.example.superbank;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import model.TransferBalance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@RestController("/balance")
+@AllArgsConstructor
+public class BalanceController {
+
+    private final BankService bankService;
+
+    @GetMapping("/{accountId}")
+    public BigDecimal getBalance(@PathVariable Long accountId){
+        return bankService.getBalance(accountId);
+    }
+
+    @PostMapping("/add")
+    public BigDecimal addMoney(@RequestBody TransferBalance transferBalance){
+        return bankService.addMoney(transferBalance.getTo(),transferBalance.getAmount());
+    }
+
+    @PostMapping("/transfer")
+    public void transfer(@RequestBody TransferBalance transferBalance){
+        bankService.makeTransfer(transferBalance);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String handle (IllegalArgumentException e ){
+        log.error(e.getMessage());
+        return "YA SSSSSS!";
+    }
+
+
+
+}
---
+++ src/main/java/model/TransferBalance.java
@@ -0,0 +1,15 @@
+package model;
+
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+@Data
+public class TransferBalance {
+
+    private Long from;
+    private Long to;
+    private BigDecimal amount;
+
+
+}
